2b8e7f643871c0a9e69ba6a28f3e3353807d59e9
JanaHAlharbi <jalharbi0172@stu.kau.edu.sa>
2026-04-18 19:18:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,35 @@ console.log("Portfolio Loaded");
 const links = document.querySelectorAll("nav a");
 
 links.forEach(link => {
-
-link.addEventListener("click", function(){
-
-console.log("Navigating to", this.getAttribute("href"));
-
+  link.addEventListener("click", function(){
+    console.log("Navigating to", this.getAttribute("href"));
+  });
 });
 
+const aboutTabs = document.querySelectorAll(".about-tab");
+const aboutContents = document.querySelectorAll(".about-content");
+
+aboutTabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    const target = tab.getAttribute("data-about");
+
+    aboutTabs.forEach(item => item.classList.remove("active"));
+    aboutContents.forEach(content => content.classList.remove("active"));
+
+    tab.classList.add("active");
+    document.getElementById(target).classList.add("active");
+  });
+});
+
+const toggleMoreBtn = document.getElementById("toggleMoreBtn");
+const moreAbout = document.getElementById("moreAbout");
+
+toggleMoreBtn.addEventListener("click", () => {
+  moreAbout.classList.toggle("show");
+
+  if (moreAbout.classList.contains("show")) {
+    toggleMoreBtn.textContent = "Show Less";
+  } else {
+    toggleMoreBtn.textContent = "Show More";
+  }
 });
